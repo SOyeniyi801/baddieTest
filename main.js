281e3f809f38b7c4a83ea89a1f8bccc37465a79f
“Sammi <naturalbeauty801@gmail.com>
2021-09-19 01:43:34 -0400

    Bug with name display but functional

---
+++ main.js
@@ -0,0 +1,36 @@
+const correctAnswers = ['A', 'B', 'A', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result')
+const nameInput = document.querySelector('input')
+
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0
+    const userInput = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    // check answers
+
+    userInput.forEach((input, i) => {
+        if (input === correctAnswers[i]) {
+            score += 25;
+        }
+        
+    });
+
+    // results
+    scroll(0,0)
+    console.log(score)
+
+    result.classList.remove('d-none')
+    console.log(nameInput.value)
+    result.querySelector('.you').innerHTML  = `${nameInput}`
+    result.querySelector('.resultOutput').textContent = `${score}%`
+    
+
+    
+
+});
